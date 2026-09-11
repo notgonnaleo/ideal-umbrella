9e7fd1fbeca44f3ad9563088975c171cd9be3039
notgonnaleo <lbruni10@gmail.com>
2026-09-11 00:55:24 -0300

    Fixed rooms.sql

--- database/migrations/rooms.sql
+++ database/migrations/rooms.sql
@@ -1,1 +1,5 @@
-select * from rooms
+CREATE TABLE rooms (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    discord_guild_id VARCHAR(64) NOT NULL,
+    discord_channel_id VARCHAR(64) NOT NULL,
+);
